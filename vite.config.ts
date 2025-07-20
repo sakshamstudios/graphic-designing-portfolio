8498039e27fb0e51a837238b9b0674d164fa2d17
Saksham Goswami <sakshamgoswami16@gmail.com>
2025-07-20 22:02:53 +0530

    fix: added base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "./", // 👈 This is very important for Vercel if you're using relative assets or router
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
